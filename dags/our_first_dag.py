41be912f1635ab05eee4fcce82d4db9f03610abf
soheil.mahdavi1362 <soheil.mahdavi1362@gmail.com>
2025-04-23 14:14:13 +0200

    BashOperator

--- dags/our_first_dag.py
+++ dags/our_first_dag.py
@@ -11,7 +11,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='our_first_dag',
+    dag_id='our_first_dag_v5',
     default_args=default_args,
     description='This is our first dag',
     start_date=datetime(2025, 7, 20, 2),
@@ -22,4 +22,20 @@ with DAG(
         bash_command="echo hello world"
     )
 
-    task1
+    task2 = BashOperator(
+        task_id= 'second_task',
+        bash_command= 'echo second task is running'
+    )
+
+    task3 = BashOperator(
+        task_id= 'third_task',
+        bash_command= "echo I am thethird task"
+    )
+
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
+
+    # task1 >> task2
+    # task1 >> task3
+
+    task1 >> [task2, task3]
